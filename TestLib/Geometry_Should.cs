77baa39475bca412cfe9b2cdddff67254c5eda1a
kirsten1333 <kirsten1333@gmail.com>
2023-04-24 21:07:06 +0500

    Добавлен проект  с тестами

---
+++ TestLib/Geometry_Should.cs
@@ -0,0 +1,16 @@
+namespace TestLib
+{
+    public class Geometry_Should
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TestLib/Usings.cs
@@ -0,0 +1,3 @@
+global using NUnit.Framework;
+global using MyGeometryLib;
+global using MyGeometryLib.Geometry;
